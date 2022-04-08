3ccbaca4a4f2a11b824da37523ef5e83144a43d4
Peterson Guo <petersonguo@gmail.com>
2022-04-08 19:52:42 -0400

    k

--- Checkers.java
+++ Checkers.java
@@ -27,4 +27,53 @@ public class Checkers
         // put your code here
         return x + y;
     }
+    
+    // Returns optimal value for current player (Initially called for root and maximizer)
+    static int minimax(int depth, int nodeIndex,
+                       Boolean maximizingPlayer,
+                       int values[], int alpha,
+                       int beta)
+    {
+        // Base Case
+        if (depth == 3)
+            return values[nodeIndex];
+     
+        if (maximizingPlayer)
+        {
+            int best = Integer.MIN_VALUE;
+     
+            // Recur for left and right children
+            for (int i = 0; i < 2; i++)
+            {
+                int val = minimax(depth + 1, nodeIndex * 2 + i,
+                                  false, values, alpha, beta);
+                best = Math.max(best, val);
+                alpha = Math.max(alpha, best);
+     
+                // Alpha Beta Pruning
+                if (beta <= alpha)
+                    break;
+            }
+            return best;
+        }
+        else
+        {
+            int best = Integer.MAX_VALUE;
+     
+            // Recur for left and right children
+            for (int i = 0; i < 2; i++)
+            {
+                 
+                int val = minimax(depth + 1, nodeIndex * 2 + i,
+                                  true, values, alpha, beta);
+                best = Math.min(best, val);
+                beta = Math.min(beta, best);
+     
+                // Alpha Beta Pruning
+                if (beta <= alpha)
+                    break;
+            }
+            return best;
+        }
+    }
 }
