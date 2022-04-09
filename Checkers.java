79935cea6b92f69ed43382aa874ee887c7f0ebbc
Peterson Guo <petersonguo@gmail.com>
2022-04-08 21:13:15 -0400

    k

--- Checkers.java
+++ Checkers.java
@@ -1,36 +1,35 @@
 import java.util.*;
+import java.io.*;
 public class Checkers  
 {
-    // instance variables - replace the example below with your own
-    private Scanner scan;
+    private ArrayList<ArrayList<Character>> row;
     private int turn = 0;
-    private ArrayList<ArrayList<Character>> row = new ArrayList<ArrayList<Character>>();
-
-    /**
-     * Constructor for objects of class Checkers
-     */
+    private Scanner scan;
+    
     public Checkers()
     {
         scan = new Scanner(System.in);
+        row = new ArrayList<ArrayList<Character>>();
         for(int i = 0; i < 8; i++)
         {
-            ArrayList<Character> col = new ArrayList<Character>();
-            for(int j = 0; j < 4; j++)
-            {
-                col.add('n');
+            ArrayList<Character> x = new ArrayList<Character>();
+            for(int j = 0; j < 4; j++){
+                x.add('n');
             }
-            row.add(col);
+            row.add(x);
         }
     }
     
-    // Returns optimal value for current player (Initially called for root and maximizer)
-        static int minimax(int depth, int nodeIndex, boolean maximizingPlayer, int values[], int alpha, int beta)
-        {
+    public int getTurn() {return turn;}
+    
+    // Remember to implement not run minimax if only one possibility for ai
+    // Returns optimal value for current player(Initially called for root and maximizer)
+    public int minimax(int depth, int nodeIndex, boolean white, int values, int alpha, int beta)
+    {
         // Base Case
-        if (depth == 0)
-            return values[nodeIndex]; 
-         
-        if (maximizingPlayer)
+        if(depth == 10) return values;
+        
+        if(white)
         {
             int best = Integer.MIN_VALUE;
             
@@ -40,7 +39,7 @@ public class Checkers
                 int val = minimax(depth + 1, nodeIndex * 2 + i, false, values, alpha, beta);
                 best = Math.max(best, val);
                 alpha = Math.max(alpha, best);
-         
+                
                 // Alpha Beta Pruning
                 if (beta <= alpha)
                     break;
@@ -50,14 +49,15 @@ public class Checkers
         else
         {
             int best = Integer.MAX_VALUE;
-         
+            
             // Recur for left and right children
             for (int i = 0; i < 2; i++)
-            {     
-                int val = minimax(depth + 1, nodeIndex * 2 + i,true, values, alpha, beta);
+            {
+                
+                int val = minimax(depth + 1, nodeIndex * 2 + i, true, values, alpha, beta);
                 best = Math.min(best, val);
                 beta = Math.min(beta, best);
-         
+                
                 // Alpha Beta Pruning
                 if (beta <= alpha)
                     break;
---
+++ GameWorld.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameWorld extends World
+{
+    public static final int WORLD_WIDTH = 400;
+    public static final int WORLD_HEIGHT = 400;
+    
+    /**
+     * Constructor for objects of class GameWorld.
+     * 
+     */
+    public GameWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(WORLD_WIDTH, WORLD_HEIGHT, 1);
+        
+        setBackground(drawBackground());
+    }
+    
+    public static GreenfootImage drawBackground() {
+        GreenfootImage bg = new GreenfootImage(WORLD_WIDTH, WORLD_HEIGHT);
+        bg.clear();
+        
+        bg.setColor(Color.BLACK);
+        bg.fill();
+        bg.setColor(Color.WHITE);
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 4; j++){
+                if(i % 2 == 0) bg.fillRect(j * 2 * WORLD_WIDTH / 8, i * WORLD_HEIGHT / 8, WORLD_WIDTH / 8, WORLD_HEIGHT / 8);
+                else bg.fillRect(j * 2 * WORLD_WIDTH / 8 + WORLD_WIDTH / 8, i * WORLD_HEIGHT / 8, WORLD_WIDTH / 8, WORLD_HEIGHT / 8);
+            }
+        }
+        
+        return bg;
+    }
+}
